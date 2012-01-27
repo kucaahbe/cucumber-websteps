2c25f5bc769b25be274ad93b71a266f3e4e9a808
Dmitry Mjakotny <dikuca@gmail.com>
2012-01-27 12:45:38 +0200

    fixed all tests

--- lib/cucumber/websteps/form_steps.rb
+++ lib/cucumber/websteps/form_steps.rb
@@ -75,7 +75,7 @@ end
 Then /^the "([^"]*)" field(?: within (.*))? should contain "([^"]*)"$/ do |field, parent, value|
   with_scope(parent) do
     field = find_field(field)
-    field_value = (field.tag_name == 'textarea') ? field.text : field.value
+    field_value = field.value
     if field_value.respond_to? :should
       field_value.should =~ /#{value}/
     else
@@ -87,7 +87,7 @@ end
 Then /^the "([^"]*)" field(?: within (.*))? should not contain "([^"]*)"$/ do |field, parent, value|
   with_scope(parent) do
     field = find_field(field)
-    field_value = (field.tag_name == 'textarea') ? field.text : field.value
+    field_value = field.value
     if field_value.respond_to? :should_not
       field_value.should_not =~ /#{value}/
     else
